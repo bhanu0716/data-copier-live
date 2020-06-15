b2c9ac5ba4a389762855fb0e601712b51059ac70
BC <bc@bcs-macbook-pro.local>
2020-06-15 00:30:43 -0400

    Added to read table list that needs to be loaded from a file

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import sys
 from config import DB_DETAILS
 
+<<<<<<< HEAD
+=======
+from util import get_tables
+
+>>>>>>> Added to read table list that needs to be loaded from a file
 
 def main():
     """Program take atleast one argument"""
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
+<<<<<<< HEAD
     print(db_details)
+=======
+    tables = get_tables('table_list')
+    for table in tables['table_name']:
+        print(table)
+>>>>>>> Added to read table list that needs to be loaded from a file
 
 
 if __name__=='__main__':
